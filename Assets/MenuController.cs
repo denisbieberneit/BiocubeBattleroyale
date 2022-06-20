3152d51e61167a7b2cbc54862f1727394bb6b07a
michaelpdizzle <michael.polom94@gmail.com>
2022-06-20 14:00:32 +0200

    emotes added

--- Assets/MenuController.cs
+++ Assets/MenuController.cs
@@ -8,18 +8,58 @@ public class MenuController : MonoBehaviour
     private GameObject characterSelection;
     [SerializeField]
     private GameObject menu;
+    [SerializeField]
+    private GameObject emotes;
+
+    [SerializeField]
+    private GameObject killEmotes;
+    [SerializeField]
+    private GameObject deathEmotes;
 
 
     public void OpenCharacterSelection()
     {
-        menu.SetActive(false);
-        characterSelection.SetActive(true);
+        OpenPage("chars");
     }
 
     public void OpenMenu()
     {
-        characterSelection.SetActive(false);
-        menu.SetActive(true);
+        OpenPage("menu");
     }
 
+    public void OpenEmoteSelection()
+    {
+        OpenPage("emotes");
+    }
+
+    private void OpenPage(string page)
+    {
+        menu.SetActive(false);
+        characterSelection.SetActive(false);
+        emotes.SetActive(false);
+
+        if (page == "chars")
+        {
+            characterSelection.SetActive(true);
+        }
+        if (page == "emotes")
+        {
+            emotes.SetActive(true);
+        }
+        if (page == "menu")
+        {
+            menu.SetActive(true);
+        }
+    }
+
+    public void ShowKillEmotes()
+    {
+        deathEmotes.SetActive(false);
+        killEmotes.SetActive(true);
+    }
+    public void ShowDeathEmotes()
+    {
+        killEmotes.SetActive(false);
+        deathEmotes.SetActive(true);
+    }
 }
